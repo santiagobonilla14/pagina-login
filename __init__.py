7f9e622c2260b25ec1ad7ce8d3e04110028a73b1
santiagobonilla14 <111590968+santiagobonilla14@users.noreply.github.com>
2022-09-29 14:33:53 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+#Cargar las configuraciones
+app.config.from_object('config.DevelopmentConfig')
+db = SQLAlchemy(app)
+
+#Importar vistas 
+from src.views.auth import auth
+app.register_blueprint(auth)
+
+from src.views.blog import blog
+app.register_blueprint(blog)
+app.add_url_rule('/', endpoint='index')
+
+db.create_all()
