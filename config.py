3f4504144f108d4f52a70efa6f7986684a2df8bb
santiagobonilla14 <111590968+santiagobonilla14@users.noreply.github.com>
2022-09-30 10:55:13 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     #Configuración de base dedatos 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "mysql://b9ebb10b43dd3a:1b0dba33@us-cdbr-east-06.cleardb.net/heroku_a47ccabc1e4924f"
-e
+
 class ProductionConfig(Config):
     DEBUG = False
 
