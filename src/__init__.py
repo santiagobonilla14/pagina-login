4513a9189e86435f5aa5079e9c8f7fe38dabf126
santiagobonilla14 <111590968+santiagobonilla14@users.noreply.github.com>
2022-09-30 14:36:23 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,18 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
 
 #Cargar las configuraciones
-app.config.from_object('config.DevelopmentConfig')
-db = SQLAlchemy(app)
+    app.config.from_object('config.DevelopmentConfig')
+    db = SQLAlchemy(app)
 
 #Importar vistas 
-from src.views.auth import auth
-app.register_blueprint(auth)
+    from src.views.auth import auth
+    app.register_blueprint(auth)
 
-from src.views.blog import blog
-app.register_blueprint(blog)
-app.add_url_rule('/', endpoint='index')
+    from src.views.blog import blog
+    app.register_blueprint(blog)
+    app.add_url_rule('/', endpoint='index')
 
-db.create_all()
+    db.create_all()
